0a157c9fbe94400c83cbec5f54689a2109deac41
dayojohn19 <dayo_john16@yahoo.com>
2021-08-25 11:30:39 +0800

    done on pip

--- application/qrcode.py
+++ application/qrcode.py
@@ -3,10 +3,30 @@ import qrcode
 from django.shortcuts import render
 
 def main(request):
-    img = qrcode.make("https://www.google.com/?client=safari")
-    # img.save("qr.png", "PNG")
-    os.system("open qr.png")
-    return render(request, 'application/earthquake.html')
+    try:
+        if request.POST == 'POST':  
+            pass  
+            # return render(request, 'application/qrcode.html',{
+            #     'message':'no thanks'
+            # })
+        else:
+            qr = request.POST["qrcode"]        
+            qr.save("qq.png", "PNG")
+            img = qrcode.make(qr)
+            # img.save("qr.png", "PNG")
+            os.system("open qq.png")
+            return render(request, 'application/qrcode.html',{
+                'message':img
+            })
+    except:
+            return render(request, 'application/qrcode.html',{
+                'message':'no thanks'
+            })
+# def main(request):
+#     img = qrcode.make("https://www.google.com/?client=safari")
+#     # img.save("qr.png", "PNG")
+#     os.system("open qr.png")
+#     return render(request, 'application/earthquake.html')
 
 
 
--- application/urls.py
+++ application/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path("earthquake/", earthquake.main, name="earthquake"),
     path("calendar/", calendar_print.calendar_print , name="calendar"),
     path("calendar/<int:y>", calendar_print.calendar_get , name="calendar_y"),
-    path("speech/", speech.main, name="speech"),
+    # path("speech/", speech.main, name="speech"),
     path("qrcode/", qrcode.main, name="qrcode")
 ]
